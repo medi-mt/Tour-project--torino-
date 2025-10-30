1eec2e1bfde47bade375d48014c7b7a441209d02
mehdi <mehdimoazemi1919@gmail.com>
2025-10-30 19:44:49 +0330

    add notFound-tour message

--- components/template/myTour/MyTour.js
+++ components/template/myTour/MyTour.js
@@ -1,13 +1,15 @@
 "use client"
-
 import { useGetUserTour } from "../../../services/query"
 import CardUserTour from "../../module/cardUserTour/CardUserTour"
+import styles from "./MyTour.module.css"
 
 function MyTour() {
+
     const { data, isPending } = useGetUserTour()
 
     if (isPending) return (<p>درحال بارگزاری...</p>)
 
+    if (!data?.title) return <p className={styles.notFound_tour}>در حال حاضر توری ندارید</p>
     return (
         <>
             {
